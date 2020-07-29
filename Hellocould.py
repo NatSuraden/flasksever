f42c0f71c98b65e14be35840abf2acc2152e4abe
INE-01 <INE-01@Admins-iMac-5.local>
2020-07-29 17:55:26 +0700

    update2

--- Hellocould.py
+++ Hellocould.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/') 
 def index():
-   return '<h1>Kello RUK-COM!</h1>'
+   return '<h1>Kello RUK-COM is Great</h1>'
 
 
 @app.route('/home', methods=['GET', 'POST']) 
